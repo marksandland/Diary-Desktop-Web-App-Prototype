0614eed39f2307c9bf4b0765a96e6053e624e941
Mark S <marksandland11@gmail.com>
2021-04-21 20:23:50 +0100

    progress

--- Project/scripts/main.js
+++ Project/scripts/main.js
@@ -13,9 +13,6 @@ function passwordReset() {
 }
 
 function createAccount() {
-
-
-    
     alert("Account created!")
     return true;
 }
@@ -23,19 +20,64 @@ function createAccount() {
 function addList() {
     var ul = document.getElementById("lists");
     var li = document.createElement("li");
+    li.className = "list"
     var a = document.createElement("a");
     a.href="#";
 
-    a.appendChild(document.createTextNode("Untitled"))
+    a.appendChild(document.createTextNode("Untitled"));
     li.appendChild(a);
+    
+
+    
+    var ulHidden = document.createElement("ul");
+    ulHidden.hidden = true;
+
+    var p = document.createElement("p");
+    p.hidden = true;
+    p.textContent = ul.children.length;
+
+    li.appendChild(ulHidden);
+    li.appendChild(p);
     ul.appendChild(li);
+    li.onclick = function() {
+        clickedList(li.getElementsByTagName("a")[0].textContent, li.getElementsByTagName("ul")[0], li.getElementsByTagName("p")[0].textContent);  
+    };
+
+
+    
+
 }
 
 function removeList() {
     alert("enter removeListMode"); 
 }
 
-window.onload = function() { 
+function clickedList(listName, list, index) {
+    var listHeading = document.getElementById("listHeading").getElementsByTagName("u")[0];
+    listHeading.textContent = listName;
+
+    var listIndexDestination = document.getElementById("listHeading").getElementsByTagName("p")[0];
+    listIndexDestination.innerHTML = index;
+
+    var listContentsDestination = document.getElementById("listContents");
+    var oldContents = listContentsDestination.getElementsByTagName("li");
+    for (var j = 0; j < oldContents.length; j++) {
+        listContentsDestination.removeChild(oldContents[j]);
+    }
+
+    var listContents = list.getElementsByTagName("li");
+    for (var i = 0; i < listContents.length; i++) {
+        var text = listContents[i].getElementsByTagName("p")[0].textContent;
+        var p = document.createElement("p");
+        p.textContent = text;
+        var li = document.createElement("li");
+        
+        li.appendChild(p);
+        listContentsDestination.appendChild(li);
+    }
+}
+
+function EventHandler() {
     //Button Click Event Handler
         //Add List Button
         var a = document.getElementById("buttonPlus");
@@ -45,9 +87,35 @@ window.onload = function() {
         var b = document.getElementById("buttonMinus");
         b.onclick = function() {removeList()};
 
+        var lists = document.getElementsByClassName("list");
 
-        
+        for (var i = 0; i < lists.length; i++ ){
+            lists[i].onclick = function() {
+                clickedList(this.getElementsByTagName("a")[0].textContent, this.getElementsByTagName("ul")[0], this.getElementsByTagName("p")[0].textContent);  
+            };
+        }
+        var heading = document.getElementById("listHeading");
+        heading.getElementsByTagName("u")[0].addEventListener("input", function() {
+            
+                var lists = document.getElementsByClassName("list");
+                for (var i = 0; i < lists.length; i++){
+
+                    var x = heading.getElementsByTagName("p")[0].textContent;
+                    var y = lists[i].getElementsByTagName("p")[0].textContent
+                    console.log(x,y);
+                    if (x==y){
+                        console.log("test");
+                        if (heading.getElementsByTagName("u")[0].textContent.length < 1) {
+                            lists[i].getElementsByTagName("a")[0].textContent = "Untitled";
+                        }
+                        else {
+                            lists[i].getElementsByTagName("a")[0].textContent = heading.getElementsByTagName("u")[0].textContent;
+                        }
+                    }
+                }
+        }, false);
 }
 
-        
-
+window.onload = function() { 
+    EventHandler();
+}
